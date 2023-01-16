3aac3013cf8b90f04dd88ee0912a7effc7036777
Vishal <mh20bu3372@gmail.com>
2023-01-17 02:54:07 +0530

    My First Spring Program - Dependency Injection

--- src/main/java/com/example/springtutorialvishal/SpringTutorialVishalApplication.java
+++ src/main/java/com/example/springtutorialvishal/SpringTutorialVishalApplication.java
@@ -1,13 +1,18 @@
 package com.example.springtutorialvishal;
 
+import com.example.springtutorialvishal.controller.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringTutorialVishalApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringTutorialVishalApplication.class, args);
+
+        ApplicationContext context = SpringApplication.run(SpringTutorialVishalApplication.class, args);
+        MyController myController = (MyController) context.getBean("myController") ;
+        System.out.println(myController.sayHello());
     }
 
 }
---
+++ src/main/java/com/example/springtutorialvishal/controller/MyController.java
@@ -0,0 +1,12 @@
+package com.example.springtutorialvishal.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello(){
+        return "Hello" ;
+    }
+}
+
+

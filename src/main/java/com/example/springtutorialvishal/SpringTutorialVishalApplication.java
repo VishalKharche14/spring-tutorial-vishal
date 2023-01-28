d16c6930fc14ae9497cee49e397e7a190977809d
Vishal <mh20bu3372@gmail.com>
2023-01-29 02:29:06 +0530

    updated

--- src/main/java/com/example/springtutorialvishal/SpringTutorialVishalApplication.java
+++ src/main/java/com/example/springtutorialvishal/SpringTutorialVishalApplication.java
@@ -1,7 +1,6 @@
 package com.example.springtutorialvishal;
 
 import com.example.springtutorialvishal.controller.ConstructorInjectedController;
-import com.example.springtutorialvishal.controller.MyController;
 import com.example.springtutorialvishal.controller.PropertyInjectedController;
 import com.example.springtutorialvishal.controller.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
@@ -14,9 +13,6 @@ public class SpringTutorialVishalApplication {
     public static void main(String[] args) {
 
         ApplicationContext context = SpringApplication.run(SpringTutorialVishalApplication.class, args);
-
-        MyController myController = (MyController) context.getBean("myController") ;
-        System.out.println(myController.sayHello());
 
         ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) context.getBean("constructorInjectedController") ;
         System.out.println(constructorInjectedController.getGreeting());
